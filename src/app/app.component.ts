3bf75cf3e335c388bfb3efcbebbc7a25f3b55405
Jean Brito <jeanbrito22@gmail.com>
2019-06-22 01:47:43 -0300

    changing the imports from name services alteration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { LoadingIndicatorService } from './loading/loading-indicator.service';
-import { AppService } from './services/app.service';
+import { HtttpRequestService } from './services/http-request.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { GenreNameService } from './services/genre-name.service';
+import { ShareGenreNameService } from './services/share-genre-name.service';
 
 @Component({
   selector: 'app-root',
@@ -13,16 +13,11 @@ export class AppComponent {
   
   protected loading: boolean = false;
   
-  constructor(protected appService: AppService<any>,
-              protected loadingIndicatorService: LoadingIndicatorService,
-              protected activatedRoute?: ActivatedRoute,
-              protected genreNameService?: GenreNameService,
-              protected router?: Router ) { 
+  constructor(protected loadingIndicatorService: LoadingIndicatorService, protected activatedRoute?: ActivatedRoute, protected router?: Router,
+              protected httpRequestService?: HtttpRequestService<any>, protected shareGenreNameService?: ShareGenreNameService) { 
                 
     // change isLoading status whenever notified
-    loadingIndicatorService
-      .onLoadingChanged
-      .subscribe(isLoading => this.loading = isLoading);
+    loadingIndicatorService.onLoadingChanged.subscribe(isLoading => this.loading = isLoading);
   }
   
 }
