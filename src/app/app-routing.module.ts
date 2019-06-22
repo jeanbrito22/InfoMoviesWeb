8fec2be818470922c216feb8e45e13a6956bcf77
Jean Brito <jeanbrito22@gmail.com>
2019-06-22 01:42:01 -0300

    treating the Topbar Component as a router outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { MoviesListComponent } from './movies/movies-list/movies-list.component';
 import { MovieComponent } from './movie/movie.component';
 import { GenreListComponent } from './genres/genre-list/genre-list.component';
+import { TopbarComponent } from './topbar-items/topbar/topbar.component';
 
 const routes: Routes = [
-  {path: 'genre/:genreId', component: MoviesListComponent},
-  {path: 'movie/:movieId', component: MovieComponent},
-  { path: '', component: GenreListComponent},
-  {path: '**', component: GenreListComponent}
-  
+  {path: 'genre/:genreId', component: MoviesListComponent },
+  {path: 'movie/:movieId', component: MovieComponent },
+  {path: '', component: GenreListComponent},
+  {path: '**', component: GenreListComponent},
+  {path: '', component: TopbarComponent, outlet: 'topbar'}
 ];
 
 @NgModule({
