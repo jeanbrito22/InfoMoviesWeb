33a35298db653e7d75881a27c3195f62ba252ab0
Jean Brito <jeanbrito22@gmail.com>
2019-06-29 14:39:48 -0300

    Route Resolvers

---
+++ src/app/movies/movies-list/movie-list.resolver.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { Movie } from 'src/app/movie/movie';
+import { AppMovieApiConfig } from 'src/app/app-movie-api-config';
+import { HttpClientService } from 'src/app/services/http-client.service';
+
+const KEY = AppMovieApiConfig.getApiKey();
+const LANGUAGE: string = AppMovieApiConfig.getLanguage();
+const API_MOVIES = `https://api.themoviedb.org/3/discover/movie?api_key=${KEY}&language=${LANGUAGE}&with_genres=`;
+
+@Injectable({ providedIn: 'root' })
+
+export class MovieListResolver implements Resolve<Observable<Movie[]>> {
+    
+
+    movies: Movie[] = [];
+
+    constructor(private httpClientService: HttpClientService<Movie>) {}
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Movie[]> {
+
+        const genreId = route.params.genreId;
+        return this.httpClientService.getData(API_MOVIES, genreId);
+    }
+
+}
